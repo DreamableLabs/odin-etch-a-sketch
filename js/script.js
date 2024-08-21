766f666c85e286950bf117d326d435f340b1ce39
Alex <1936228+DreamableLabs@users.noreply.github.com>
2024-08-21 16:12:21 -0400

    feat: add button to allow user to specify squares per side
    Added button that allows user to specify squares per
    side. If a valid integer between 1 and 100 is entered,
    all existing child divs of grid are removed and new
    ones are generated.

--- js/script.js
+++ js/script.js
@@ -1,33 +1,55 @@
-const SQUARES_PER_SIDE = 16;
+let squaresPerSide = 16;
 
 const grid = document.querySelector('#grid');
 const gridHeight = grid.clientHeight;
-console.log(gridHeight);
-const squareHeight = gridHeight / SQUARES_PER_SIDE;
-
 const squares = [];
 const rows = [];
-for (let row = 0; row < SQUARES_PER_SIDE; row++) {
-    squares[row] = [];
-    rows[row] = document.createElement('div');
-    for (let col = 0; col < SQUARES_PER_SIDE; col++) {
-        squares[row][col] = document.createElement('div');
-        rows[row].appendChild(squares[row][col]);
+
+function generateGridSquares() {
+    const squareHeight = gridHeight / squaresPerSide;
+
+    for (let row = 0; row < squaresPerSide; row++) {
+        squares[row] = [];
+        rows[row] = document.createElement('div');
+        for (let col = 0; col < squaresPerSide; col++) {
+            squares[row][col] = document.createElement('div');
+            rows[row].appendChild(squares[row][col]);
+        }
+        grid.appendChild(rows[row]);
     }
-    grid.appendChild(rows[row]);
+
+    for (let row of rows) {
+        row.classList.add('row');
+    }
+
+    for (let squareRow of squares) {
+        for (let square of squareRow) {
+            square.style.width = `${squareHeight}px`;
+            square.style.height = `${squareHeight}px`;
+            square.classList.add('square');
+            square.addEventListener('mouseover', function() {
+                this.style.background = 'black';
+            });
+        }
+    }
 }
 
-for (let row of rows) {
-    row.classList.add('row');
-}
-
-for (let squareRow of squares) {
-    for (let square of squareRow) {
-        square.style.width = `${squareHeight}px`;
-        square.style.height = `${squareHeight}px`;
-        square.classList.add('square');
-        square.addEventListener('mouseover', function() {
-            this.style.background = 'black';
-        });
+function clearGrid() {
+    while (grid.firstChild) {
+        grid.removeChild(grid.firstChild);
     }
 }
+
+generateGridSquares();
+const button = document.querySelector('#btn');
+button.addEventListener('click', () => {
+    let userInput = prompt('How many squares would you like per side?', 0);
+    let squaresPerSideRequested = parseInt(userInput.trim(), 10);
+    if (!isNaN(squaresPerSideRequested) && squaresPerSideRequested <= 100 && squaresPerSideRequested > 0) {
+        squaresPerSide = squaresPerSideRequested;
+        clearGrid();
+        generateGridSquares();
+    } else {
+        alert('Invalid input! Please enter an integer value between 1 and 100.')
+    }
+});
