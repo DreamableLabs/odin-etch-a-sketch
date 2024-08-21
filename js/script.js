b3c03f4f5eaac1a0caa04cacbf4b74a0606abf92
Alex <1936228+DreamableLabs@users.noreply.github.com>
2024-08-21 15:35:08 -0400

    chore(cleanup): style rows and squares in dedicated loops

--- js/script.js
+++ js/script.js
@@ -10,11 +10,21 @@ const rows = [];
 for (let row = 0; row < SQUARES_PER_SIDE; row++) {
     squares[row] = [];
     rows[row] = document.createElement('div');
-    rows[row].classList.add('row');
     for (let col = 0; col < SQUARES_PER_SIDE; col++) {
         squares[row][col] = document.createElement('div');
-        squares[row][col].classList.add('square');
         rows[row].appendChild(squares[row][col]);
     }
     grid.appendChild(rows[row]);
+}
+
+for (let row of rows) {
+    row.classList.add('row');
+}
+
+for (let squareRow of squares) {
+    for (let square of squareRow) {
+        square.style.width = `${squareHeight}px`;
+        square.style.height = `${squareHeight}px`;
+        square.classList.add('square');
+    }
 }
